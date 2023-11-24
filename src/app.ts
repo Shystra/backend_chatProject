47838ae734884660ff861b55a07685bd231b125c
Shystra <jlsp.2019@hotmail.com>
2023-11-23 21:51:49 -0300

    feate: just struture for new server usiing socket.io

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+import http from 'http';
+import express, { Application } from 'express';
+import { Server } from 'socket.io';
+
+
+
+class App {
+    private app: Application
+    private http: http.Server
+    private io: Server;
+
+
+    constructor(){
+        this.app = express()
+        this.http = new http.Server(this.app)
+        this.io = new Server(this.http);
+        this.initializeHtml();
+    }
+
+
+    listen(){
+        this.http.listen(3333, () => console.log('Server is running'))
+    }
+
+    listenSocket(){
+        this.io.on('connection', (userSocket) => {
+            console.log('a user connected');
+        });
+    }
+    private initializeHtml(){
+        this.app.get('/index', (req, res) => {
+            console.log('HTML is running');
+            res.sendFile(__dirname + '/index.html')
+        });
+    }
+}
+
+export { App };
